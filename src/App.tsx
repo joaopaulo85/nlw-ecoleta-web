62792abd88c2157e10287bb8c6e034a7f5a47138
joaopaulo <jptick@gmail.com>
2020-09-03 16:37:35 -0300

    Create Component Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import Header from './Header';
+
 function App() {
   return (
-       <h1>Hello World</h1>
+       <div>
+         <Header title="Ecoleta"/>
+         <h1>Conteúdo da aplicação</h1>
+       </div>
   );
 }
 
